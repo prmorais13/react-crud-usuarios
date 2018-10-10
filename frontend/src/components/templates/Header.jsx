04086eef233a10dba2379d38bab883b4560af9a8
prmorais13 <prmorais_13@hotmail.com>
2018-10-10 15:51:41 -0300

    233. Criando Componentes (Template)

---
+++ frontend/src/components/templates/Header.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+import "./Header.css";
+
+export default props => <header className="header">Cabeçalho</header>;
---
+++ frontend/src/components/templates/Logo.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+import "./Logo.css";
+
+export default props => <aside className="logo">Logo</aside>;
