ac71436b3aea9f447aa636850c49c58666776f0d
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-10-10 22:10:18 -0300

    235. Componente Logo

--- frontend/src/components/templates/Logo.jsx
+++ frontend/src/components/templates/Logo.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React from 'react';
 
-import "./Logo.css";
+import './Logo.css';
+import logo from '../../assets/img/logo.png';
 
-export default props => <aside className="logo">Logo</aside>;
+export default props => (
+  <aside className="logo">
+    <a href="/" className="logo">
+      <img src={logo} alt="logo" />
+    </a>
+  </aside>
+);
