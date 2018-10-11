181a513110328a71ac3a0ea7ee8aaca728ae2b5a
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-10-10 22:55:34 -0300

    238. Componente Navegação

--- frontend/src/components/templates/Nav.jsx
+++ frontend/src/components/templates/Nav.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import React from 'react';
 
-import "./Nav.css";
+import './Nav.css';
 
-export default props => <aside className="menu-area">Navegação</aside>;
+export default props => (
+  <aside className="menu-area">
+    <nav className="menu">
+      <a href="#/">
+        <i className="fa fa-home" /> Início
+      </a>
+      <a href="#/users">
+        <i className="fa fa-users" /> Usuários
+      </a>
+    </nav>
+  </aside>
+);
