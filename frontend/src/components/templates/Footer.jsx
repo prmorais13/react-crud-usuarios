4b223cad1b41468a21830309f95c7d001a9b3fcb
Paulo Roberto <pauloroberto@natal.rn.gov.br>
2018-10-10 22:42:42 -0300

    237. Componente Rodapé

--- frontend/src/components/templates/Footer.jsx
+++ frontend/src/components/templates/Footer.jsx
@@ -1,5 +1,13 @@
-import React from "react";
+import React from 'react';
 
-import "./Footer.css";
+import './Footer.css';
 
-export default props => <footer className="footer">Rodapé</footer>;
+export default props => (
+  <footer className="footer">
+    Desenvolvido com <i className="fa fa-heart text-danger" /> por
+    <strong>
+      {' '}
+      Cod <span className="text-danger">3</span>r
+    </strong>
+  </footer>
+);
